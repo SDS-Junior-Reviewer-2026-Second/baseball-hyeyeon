5627c7c8a6f443450491911e7166f1bdd6a86e28
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-18 11:25:36 +0900

    0~9 문자열만 허용하도록 green green~~

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,5 +10,11 @@ public class Game {
             throw new IllegalArgumentException();
         }
 
+        for(char number : guessNumber.toCharArray()) {
+            if(number < '0' || number > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+
     }
 }
