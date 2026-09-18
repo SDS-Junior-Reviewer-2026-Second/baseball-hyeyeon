89597a060f9ab88a6e58505ca0f5850216aa1609
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-18 11:28:18 +0900

    refactoring해도 잘되는지 green green~~

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,15 @@ package baseball;
 public class Game {
 
     public void guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+
+        if(isDuplicatedNumber(guessNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    private static void assertIllegalArgument(String guessNumber) {
         if(guessNumber == null){
             throw new IllegalArgumentException();
         }
@@ -15,12 +24,11 @@ public class Game {
                 throw new IllegalArgumentException();
             }
         }
+    }
 
-        if(guessNumber.charAt(0) == guessNumber.charAt(1)
-        || guessNumber.charAt(0) == guessNumber.charAt(2)
-        || guessNumber.charAt(1) == guessNumber.charAt(2)) {
-            throw new IllegalArgumentException();
-        }
-
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
